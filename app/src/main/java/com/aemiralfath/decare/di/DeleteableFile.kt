22069e8b16776f1ba274f6fe01a5ccd6903c457a
dharmayudistira <dharmayudistira2000@gmail.com>
2021-05-10 15:10:26 +0700

    pembuatan struktur folder

---
+++ app/src/main/java/com/aemiralfath/decare/di/DeleteableFile.kt
@@ -0,0 +1,8 @@
+package com.aemiralfath.decare.di
+
+/*
+    Ini cuman delete-able file isinya cuman comment, soalnya kalo foldernya kosong nanti github gabakal add
+    bisa langsung delete aja
+
+    Folder ini untuk depedency injection nanti
+ */
---
+++ app/src/main/java/com/aemiralfath/decare/util/DeteleableFile.kt
@@ -0,0 +1,6 @@
+package com.aemiralfath.decare.util
+
+/*
+    Ini cuman delete-able file isinya cuman comment, soalnya kalo foldernya kosong nanti github gabakal add
+    bisa langsung delete aja
+ */
