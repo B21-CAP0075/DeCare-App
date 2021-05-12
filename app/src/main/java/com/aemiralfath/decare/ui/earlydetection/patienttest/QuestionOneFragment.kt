61e5a1897c39091662ea93996dbd5bf051f4f39f
dharmayudistira <dharmayudistira2000@gmail.com>
2021-05-12 16:09:06 +0700

    migrating to navigation component

--- app/src/main/java/com/aemiralfath/decare/ui/earlydetection/patienttest/QuestionOneFragment.kt
+++ app/src/main/java/com/aemiralfath/decare/ui/earlydetection/patienttest/QuestionOneFragment.kt
@@ -33,31 +33,30 @@ class QuestionOneFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.btnNextQuestionOne.setOnClickListener {
-//            if (isValidate()) {
-//                findNavController().navigate(R.id.action_questionOneFragment_to_questionTwoFragment)
-//            }
-            findNavController().navigate(R.id.action_questionOneFragment_to_questionTwoFragment)
+            if (isValidate()) {
+                findNavController().navigate(R.id.action_questionOneFragment_to_questionTwoFragment)
+            }
         }
     }
 
-//    private fun isValidate(): Boolean {
-//        if (TextUtils.isEmpty(binding.edtTimeQuestionOne.editText?.text)) {
-//            binding.edtTimeQuestionOne.error = resources.getString(R.string.empty_error_field_message)
-//            return false
-//        }
-//        if (TextUtils.isEmpty(binding.edtDaysQuestionOne.editText?.text)) {
-//            binding.edtDaysQuestionOne.error = resources.getString(R.string.empty_error_field_message)
-//            return false
-//        }
-//        if (TextUtils.isEmpty(binding.edtMonthQuestionOne.editText?.text)) {
-//            binding.edtMonthQuestionOne.error = resources.getString(R.string.empty_error_field_message)
-//            return false
-//        }
-//        if (TextUtils.isEmpty(binding.edtYearQuestionOne.editText?.text)) {
-//            binding.edtYearQuestionOne.error = resources.getString(R.string.empty_error_field_message)
-//            return false
-//        }
-//
-//        return true
-//    }
+    private fun isValidate(): Boolean {
+        if (TextUtils.isEmpty(binding.edtTimeQuestionOne.editText?.text)) {
+            binding.edtTimeQuestionOne.error = resources.getString(R.string.empty_error_field_message)
+            return false
+        }
+        if (TextUtils.isEmpty(binding.edtDaysQuestionOne.editText?.text)) {
+            binding.edtDaysQuestionOne.error = resources.getString(R.string.empty_error_field_message)
+            return false
+        }
+        if (TextUtils.isEmpty(binding.edtMonthQuestionOne.editText?.text)) {
+            binding.edtMonthQuestionOne.error = resources.getString(R.string.empty_error_field_message)
+            return false
+        }
+        if (TextUtils.isEmpty(binding.edtYearQuestionOne.editText?.text)) {
+            binding.edtYearQuestionOne.error = resources.getString(R.string.empty_error_field_message)
+            return false
+        }
+
+        return true
+    }
 }
